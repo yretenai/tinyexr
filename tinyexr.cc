2e231a716da2f4fca5ccfebd816976e1b1675d19
Ada <yretenai@gmail.com>
2025-02-28 00:35:34 +0000

    add meson build for static/shared library generation

--- tinyexr.cc
+++ tinyexr.cc
@@ -4,5 +4,20 @@
 #endif
 #endif
 
+#if TINYEXR_USE_MINIZ==1
+// included by tinyexr.h
+#elif TINYEXR_USE_NANOZLIB==1
+// included by tinyexr.h
+#elif TINYEXR_USE_STB_ZLIB==1
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+#elif TINYEXR_USE_ZLIBNG==1
+#include <zlib-ng.h>
+#else
+#include <zlib.h>
+#endif
+
 #define TINYEXR_IMPLEMENTATION
 #include "tinyexr.h"
